85dc44c6c252dafc9bbb04d13417799fbfb50e6e
Shun Taguchi <taguccis2001@gmail.com>
2024-06-25 19:41:06 +0900

    コントラスト変化

---
+++ image_edit/change_contrast.py
@@ -0,0 +1,13 @@
+import cv2
+
+def change_contrast(image, alpha, beta):
+
+    # Change contrast
+    new_image = cv2.convertScaleAbs(image, alpha=alpha, beta=beta)
+
+    return new_image
+
+img = cv2.imread("../blender/dataset-example/20240623_134443_96x96/train/input/4.png")
+new_img = change_contrast(img, 10, 0)
+# cv2.imshow('Image', new_img)
+cv2.imwrite('output.png', new_img)
